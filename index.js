0f3897e99b779c0ad8155c9b4b8ac957f07950c2
denny_svg <sarah.wanjiku@student.moringaschool.com>
2023-12-27 11:18:45 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Write a reduce function.
+function reduce(batteries) {
+    return batteries.reduce((acc, current) => acc + current, 0);
+}
+const batteries = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = reduce(batteries);
+console.log (totalBatteries);
